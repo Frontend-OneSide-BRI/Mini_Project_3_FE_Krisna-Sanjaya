8db589852aa0030ecf8b149cea0c5b36b5411279
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 22:00:00 +0700

    add handle redirect to detail page on card

--- src/components/Molecule/Card/index.jsx
+++ src/components/Molecule/Card/index.jsx
@@ -1,16 +1,14 @@
 
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import { CardActionArea, Grid } from "@mui/material";
 import { useState } from "react";
 import { REACT_BASE_IMG_URL } from "../../../constants";
+import { useNavigate } from 'react-router-dom';
 
 export function CardDetails({ data }) {
-  console.log(data)
   return (
     <Card sx={{ width: { lg: "50%", xs: '80%' }, borderRadius: "14px", backgroundColor: 'black' }}>
       <CardMedia
@@ -18,26 +16,31 @@ export function CardDetails({ data }) {
         image={REACT_BASE_IMG_URL + data}
         title="green iguana"
       />
-    </Card>
+    </Card >
   );
 }
 
 export function CardMovies({ data }) {
+  const navigate = useNavigate();
+
   return (
     <Grid item xs={12} sm={6} md={4} lg={2} sx={{ mb: 2 }}>
       <Card sx={{ width: "100%", height: "100%", borderRadius: "14px" }}>
-        <CardMedia
-          sx={{ height: "40vh" }}
-          image={REACT_BASE_IMG_URL + data.poster_path}
-          title="green iguana"
-        />
+        <CardActionArea onClick={() => navigate('/detail/' + data.id)}>
+          <CardMedia
+            sx={{ height: "40vh" }}
+            image={REACT_BASE_IMG_URL + data.poster_path}
+            title="green iguana"
+          />
+        </CardActionArea>
       </Card>
-    </Grid>
+    </Grid >
   );
 }
 
 export function CardText({ data }) {
   const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -52,30 +55,32 @@ export function CardText({ data }) {
       <Card sx={{ width: "100%", height: "100%", borderRadius: "10px", backgroundColor: 'black' }}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}>
-        <CardMedia
-          sx={{
-            height: "20vh",
-            transition: 'opacity 1s',
-            borderRadius: '14px'
-          }}
-          image={REACT_BASE_IMG_URL + data?.backdrop_path}
-          title="green iguana"
-        />
-        <CardContent
-          sx={{
-            backgroundColor: "#16181F",
-            transition: "opacity 1s",
-            opacity: isHovered ? 1 : 0,
-            visibility: isHovered ? "visible" : "hidden",
-            height: isHovered ? 'auto' : '0px'
-          }}>
-          <Typography gutterBottom variant="h5" component="div" color="white">
-            {data?.original_title}
-          </Typography>
-          <Typography variant="body2" color="white">
-            {data?.overview}
-          </Typography>
-        </CardContent>
+        <CardActionArea onClick={() => navigate('/detail/' + data.id)}>
+          <CardMedia
+            sx={{
+              height: "20vh",
+              transition: 'opacity 1s',
+              borderRadius: '14px'
+            }}
+            image={REACT_BASE_IMG_URL + data?.backdrop_path}
+            title="green iguana"
+          />
+          <CardContent
+            sx={{
+              backgroundColor: "#16181F",
+              transition: "opacity 1s",
+              opacity: isHovered ? 1 : 0,
+              visibility: isHovered ? "visible" : "hidden",
+              height: isHovered ? 'auto' : '0px'
+            }}>
+            <Typography gutterBottom variant="h5" component="div" color="white">
+              {data?.original_title}
+            </Typography>
+            <Typography variant="body2" color="white">
+              {data?.overview}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
   );
