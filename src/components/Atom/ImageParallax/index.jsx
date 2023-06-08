990047d4a8ab3d66b4763deae5a90f3616fd5e9d
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 09:26:03 +0700

    add parallax image component

---
+++ src/components/Atom/ImageParallax/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+export default function ImageParallax() {
+    return (
+        <div className="parallax" data-speed="2">
+            <img src='/Images/1.png' alt="Parallax Image" />
+        </div>
+    )
+}
