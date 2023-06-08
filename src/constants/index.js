7b633ccc75711b57a106fd4bad2c0b5fb4f30ccc
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 17:05:16 +0700

    add constanta variable

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const REACT_BASE_URL = "https://api.themoviedb.org/3";
+export const REACT_BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
+export const API_KEY = "cd23c6a2cd401bc41eae6f83915084c6";
