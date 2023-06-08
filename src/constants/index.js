8a6aa131e5b96c43ab6151c940c0993a78530327
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 23:53:18 +0700

    update api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const REACT_BASE_URL = "https://api.themoviedb.org/3";
 export const REACT_BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
-export const API_KEY = "cd23c6a2cd401bc41eae6f83915084c6";
+export const API_KEY = "6c15fe2159927b024a76120507ef66de";
