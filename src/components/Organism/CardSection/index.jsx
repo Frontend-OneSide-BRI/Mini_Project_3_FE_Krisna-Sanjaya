b2ff15dd8fed6ad62cbd0bb10be90bea9c5c44cd
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 18:11:00 +0700

    fix bug when image is null

--- src/components/Organism/CardSection/index.jsx
+++ src/components/Organism/CardSection/index.jsx
@@ -3,17 +3,17 @@ import { Grid, Typography } from '@mui/material'
 import { CardMovies, CardText } from '../../Molecule/Card'
 
 
-export default function CardSection({ text, isText = false, data }) {
-    console.log(isText)
+export default function CardSection({ text, isText = false, data, max = null }) {
+    console.log(data)
     return (
         <>
             <Typography variant="h4" sx={{ color: 'white', display: 'block', my: 2, fontWeight: 'bold' }}>{text}</Typography>
             <Grid container spacing={3} column={12}>
                 {
                     isText ? (
-                        data?.map((item, i) => i < 4 && <CardText data={item} />)
+                        data?.map((item, i) => (i < (max ?? 4) && item.poster_path) && < CardText data={item} />)
                     ) : (
-                        data?.map((item, i) => i < 6 && <CardMovies data={item} />)
+                        data?.map((item, i) => (i < (max ?? 6) && item.poster_path) && <CardMovies data={item} />)
                     )
                 }
             </Grid>
