8863e8acf57ebbd27d3069dd229c5a3c6da32b6c
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 17:02:32 +0700

    add provider on redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
 
 import Router from './Routes/route';
+import { store } from './app/store';
 
 
 function App() {
   return (
-    <Router/>
+    <Provider store={store}>
+      <Router />
+    </Provider>
   );
 }
 
