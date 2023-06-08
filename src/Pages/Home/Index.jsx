19d8172cab269fe9eb34bcdb664d3693c87f448e
Krisna Sanjaya <58094843+krisnassjy@users.noreply.github.com>
2023-06-08 09:49:11 +0700

    refactor parallax component

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,34 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
-import Nav from '../../components/Molecule/Nav';
-import ImageParallax from '../../components/Atom/ImageParallax';
-import CardSection from '../../components/Organism/CardSection';
+import { Nav } from '../../components/Molecule'
+import { ImageParallax } from '../../components/Atom'
+import CardSection from '../../components/Organism/CardSection'
 
 import { Grid } from '@mui/material'
 
 import './index.css'
 
-
 export default function Home() {
-
-    useEffect(() => {
-        const parallax = document.querySelector('.parallax');
-        const image = document.querySelector('.parallax img');
-
-        const handleParallax = () => {
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            const yPos = scrollTop / parallax.dataset.speed;
-
-            image.style.transform = `translate3d(0px, ${yPos}px, 0px)`;
-        };
-
-        window.addEventListener('scroll', handleParallax);
-
-        return () => {
-            window.removeEventListener('scroll', handleParallax);
-        };
-    }, []);
-
     return (
         <Grid container sx={{ height: '100vh' }}>
             <Grid xs={1} sx={{ backgroundColor: 'black', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
--- src/components/Atom/ImageParallax/index.jsx
+++ src/components/Atom/ImageParallax/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './index.css'
 
 export default function ImageParallax() {
+    useEffect(() => {
+        const parallax = document.querySelector('.parallax');
+        const image = document.querySelector('.parallax img');
+
+        const handleParallax = () => {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const yPos = scrollTop / parallax.dataset.speed;
+
+            image.style.transform = `translate3d(0px, ${yPos}px, 0px)`;
+        };
+
+        window.addEventListener('scroll', handleParallax);
+
+        return () => {
+            window.removeEventListener('scroll', handleParallax);
+        };
+    }, []);
+
     return (
         <div className="parallax" data-speed="2">
             <img src='/Images/1.png' alt="Parallax Image" />
